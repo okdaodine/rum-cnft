7562a7555d216b9a77705af0813a5cc3eb30690f
okdaodine <btc.link.grt@gmail.com>
2023-04-04 08:38:11 +0800

    tweaks

--- server/pollingContent/handlePost.js
+++ server/pollingContent/handlePost.js
@@ -33,7 +33,8 @@ const pack = async item => {
         name,
         content,
         image,
-      }
+      },
+      published,
     },
     SenderPubkey,
     TimeStamp,
@@ -48,7 +49,7 @@ const pack = async item => {
     storage: 'chain',
     commentCount: 0,
     likeCount: 0,
-    timestamp: parseInt(String(TimeStamp / 1000000), 10)
+    timestamp: published ? new Date(published).getTime() : parseInt(TimeStamp.slice(0, 13)),
   }
   if (image) {
     const images = Array.isArray(image) ? image : [image];
